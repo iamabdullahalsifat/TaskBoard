c2029a6f6843b4a11662d2546a124180c70714f1
Mark-Abdullah <hsabdullah365@gmail.com>
2025-03-02 01:33:37 +0600

    Real Time Added

--- script/script.js
+++ script/script.js
@@ -1,6 +1,8 @@
 // Variable Store
 let mainBody = document.getElementById('main_body');
 let rendomColorBtn = document.getElementById('rendom_color_btn');
+let discoverSomethingNewToday = document.querySelector('.discover_something_new_today');
+let todayDate = document.getElementById("today_date");
 
 // Random Color Generator
 rendomColorBtn.addEventListener('click', () => {
@@ -12,3 +14,20 @@ function rgbColor(){
   let blue = Math.floor(Math.random() * 255);
   return `rgb(${red}, ${green}, ${blue})`
 }
+// Discover Something New Today
+discoverSomethingNewToday.addEventListener('click', () =>{
+  window.location.href = "../faq/index.html"
+})
+// Today Date Update
+function getTodayDate(){
+  const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+  const d = new Date();
+  const dayName = days[d.getDay()];
+  const monthName = months[d.getMonth()];
+  const date = d.getDate();
+  const year = d.getFullYear();
+  return `${dayName},<br> <b>${monthName} ${date} ${year}</b>`;
+}
+todayDate.innerHTML = getTodayDate();
+todayDate.style.fontSize = "19px";
